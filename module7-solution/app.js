5d4892837c105bd742d5a21277e53f41eac74d58
Kristine Zhang <kristinez@berkeley.edu>
2023-01-31 18:50:13 -0800

    clean up

--- module7-solution/app.js
+++ module7-solution/app.js
@@ -2,68 +2,47 @@
 'use strict';
 
 angular.module('ShoppingListApp', [])
-.controller('ShoppingListAddController', ShoppingListAddController)
-.controller('ShoppingListShowController', ShoppingListShowController)
-.controller('ShoppingListShowController2', ShoppingListShowController2)
-.service('ShoppingListService', ShoppingListService);
-
-ShoppingListAddController.$inject = ['ShoppingListService'];
-function ShoppingListAddController(ShoppingListService) {
-  var itemAdder = this;
-
-  itemAdder.itemName = "";
-  itemAdder.itemQuantity = "";
-
-  itemAdder.addItem = function () {
-    ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
-  }
-}
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 
-ShoppingListShowController.$inject = ['ShoppingListService'];
-function ShoppingListShowController(ShoppingListService) {
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
   var showList = this;
 
-  showList.items = ShoppingListService.getItems();
+  showList.items = ShoppingListCheckOffService.getItems();
 
   showList.removeItem = function (itemIndex, selected) {
     try {
-      ShoppingListService.removeItem(itemIndex, selected);
+      ShoppingListCheckOffService.removeItem(itemIndex, selected);
     } catch (error) {
       showList.errorMessage = error.message;
     }
   };
 }
 
-ShoppingListShowController2.$inject = ['ShoppingListService'];
-function ShoppingListShowController2(ShoppingListService) {
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
   var showList = this;
 
-  showList.items = ShoppingListService.getItems2();
+  showList.items = ShoppingListCheckOffService.getItems2();
   showList.errorMessage = "Nothing bought yet.";
 }
 
 
-function ShoppingListService() {
+function ShoppingListCheckOffService() {
   var service = this;
 
   // List of shopping items
   var items = [
-                {name: "cookie1", quantity: "10", price: "10"}, 
-                {name: "cookie2", quantity: "10", price: "20"}, 
-                {name: "cookie3", quantity: "10", price: "30"},
-                {name: "cookie4", quantity: "10", price: "50"},
-                {name: "cookie5", quantity: "10", price: "80"}
+                {name: "cookiesBrandA", quantity: "10", pricePerItem: "10"},
+                {name: "cookiesBrandB", quantity: "10", pricePerItem: "20"},
+                {name: "cookiesBrandC", quantity: "10", pricePerItem: "30"},
+                {name: "cookiesBrandD", quantity: "10", pricePerItem: "50"},
+                {name: "cookiesBrandE", quantity: "10", pricePerItem: "80"}
               ];
   var items2 = [];
-
-  // service.addItem = function (itemName, quantity, price) {
-  //   var item = {
-  //     name: itemName,
-  //     quantity: quantity
-  //   };
-  //   items.push(item);
-  // };
 
   service.removeItem = function (itemIndex, num_selected) {
     var temp1 = items[itemIndex];
