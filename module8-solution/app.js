f06e73de9a0ada4fa3b57c9ec33b7792a4cab96f
Kristine Zhang <kristinez@berkeley.edu>
2023-02-02 19:22:55 -0800

    Add directive for removal of an item using lecture 30 code

--- module8-solution/app.js
+++ module8-solution/app.js
@@ -4,13 +4,30 @@
 angular.module('MenuCategoriesApp', [])
 .controller('MenuCategoriesController', MenuCategoriesController)
 .service('MenuCategoriesService', MenuCategoriesService)
-.constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com");
+.constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com")
+.directive('shoppingList', ShoppingListDirective);
 
+function ShoppingListDirective() {
+  var ddo = {
+    templateUrl: 'shoppingList.html',
+    scope: {
+      items: '<',
+      badRemove: '=',
+      onRemove: '&'
+    },
+    controller: MenuCategoriesController,
+    controllerAs: 'menu',
+    bindToController: true
+  };
+
+  return ddo;
+}
 
 MenuCategoriesController.$inject = ['MenuCategoriesService'];
 function MenuCategoriesController(MenuCategoriesService) {
   var menu = this;
 
+  menu.temp1 = MenuCategoriesService.getItems();
   menu.logMenuItems = function (shortName) {
     console.log("shortName", shortName);
 
@@ -18,11 +35,23 @@ function MenuCategoriesController(MenuCategoriesService) {
 
     promise.then(function (response) {
       console.log("response", response);
-      menu.temp1 = response;
+      // menu.temp1 = response;
     })
     .catch(function (error) {
       console.log("Something went terribly wrong.");
     });
+
+    // promise.then(function (response) {
+    //   console.log(response.data);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // })
+  };
+
+  menu.removeItem = function (itemIndex) {
+    console.log("here4");
+    MenuCategoriesService.removeItem(itemIndex);
   };
 
 }
@@ -31,21 +60,26 @@ function MenuCategoriesController(MenuCategoriesService) {
 MenuCategoriesService.$inject = ['$http', 'ApiBasePath'];
 function MenuCategoriesService($http, ApiBasePath) {
   var service = this;
+  var foundItems = [];
 
   service.getMenuForCategory = function (shortName) {
+    console.log("shortName2", shortName);
     return $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json"),
     }).then(function (result) {
       // process result and only keep items that match
-      var foundItems = [];
+      console.log("result here", result);
 
       for (let key in result?.data) {
+        // console.log(key, result?.data[catagory]);
         var catagory = result?.data[key]
+        // console.log("result?.data[catagory].menu_items.length",  catagory.menu_items.length);
         for(var i = 0; i < catagory.menu_items.length; i++) {
+          // console.log("catagory.menu_items[i]", catagory.menu_items[i]);
           var description = catagory.menu_items[i].description;
           if (description.indexOf(shortName) !== -1) {
-            console.log("description", description);
+            // console.log("description", description);
             var newItem = {
               name: catagory.menu_items[i].name,
               description: catagory.menu_items[i].description,
@@ -55,12 +89,22 @@ function MenuCategoriesService($http, ApiBasePath) {
           }
         }
       }
-      
+
       // return processed items
       return foundItems;
   });
   };
 
+  service.removeItem = function (itemIndex) {
+    console.log("here5");
+    foundItems.splice(itemIndex, 1);
+  };
+
+  service.getItems = function () {
+    console.log("here3");
+
+    return foundItems;
+  };
 }
 
 })();
