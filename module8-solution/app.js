41342f88717e2cd49c205d3823da4c4c602bb14c
Kristine Zhang <kristinez@berkeley.edu>
2023-02-03 16:47:18 -0800

    clean up

--- module8-solution/app.js
+++ module8-solution/app.js
@@ -27,35 +27,22 @@ NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var menu = this;
 
-  // menu.found = MenuSearchService.getItems();
   menu.found = [];
   menu.getMatchedMenuItems = function (searchTerm) {
-    console.log("searchTerm", searchTerm);
 
     var promise = MenuSearchService.getMenuForCategory(searchTerm);
 
     promise.then(function (response) {
-      console.log("response", response);
       menu.found = response;
       menu.errorMessage = "";
     })
     .catch(function (error) {
       menu.errorMessage = error.message;
       menu.found = [];
-      console.log("error.message", error.message);
-      console.log("Something went terribly wrong.");
     });
-
-    // promise.then(function (response) {
-    //   console.log(response.data);
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // })
   };
 
   menu.removeItem = function (itemIndex) {
-    console.log("here4");
     menu.found.splice(itemIndex, 1);
   };
 
@@ -73,18 +60,12 @@ function MenuSearchService($http, ApiBasePath) {
       method: "GET",
       url: (ApiBasePath + "/menu_items.json"),
     }).then(function (result) {
-      // process result and only keep items that match
-      console.log("result here", result);
 
       for (let key in result?.data) {
-        // console.log(key, result?.data[catagory]);
         var catagory = result?.data[key]
-        // console.log("result?.data[catagory].menu_items.length",  catagory.menu_items.length);
         for(var i = 0; i < catagory.menu_items.length; i++) {
-          // console.log("catagory.menu_items[i]", catagory.menu_items[i]);
           var description = catagory.menu_items[i].description;
           if (description.indexOf(searchTerm) !== -1) {
-            // console.log("description", description);
             var newItem = {
               name: catagory.menu_items[i].name,
               short_name: catagory.menu_items[i].short_name,
@@ -98,7 +79,7 @@ function MenuSearchService($http, ApiBasePath) {
       if (foundItems.length===0) {
         throw new Error ("Nothing found");
       }
-      // return processed items
+
       return foundItems;
   });
   };
