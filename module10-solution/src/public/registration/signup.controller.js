b484089ba5b9d4cd3f8398cdfd76b7bd3f5e6540
Kristine Zhang <kristinez@berkeley.edu>
2023-02-05 14:46:53 -0800

    Add service using lec 20 code, add css using lec 42 code

--- module10-solution/src/public/registration/signup.controller.js
+++ module10-solution/src/public/registration/signup.controller.js
@@ -2,16 +2,33 @@
 "use strict";
 
 angular.module('public')
-.controller('SignupController', SignupController);
+.controller('SignupController', SignupController)
+.service('ShoppingListService', ShoppingListService);
 
-SignupController.$inject = ['menuCategories'];
-function SignupController() {
+SignupController.$inject = ['ShoppingListService'];
+function SignupController(ShoppingListService) {
   // this.user = user;
 
   this.submit = function() {
     console.log("here in register");
+    ShoppingListService.addItem(this.user);
   }
 }
 
+function ShoppingListService() {
+  var service = this;
+  console.log("here in service");
+  // List of shopping items
+  var items = [];
+  console.log("items", items)
 
+  service.addItem = function (user) {
+    console.log("here in addItem: user", user)
+    items.push(user);
+  };
+
+  service.getItems = function () {
+    return items;
+  };
+}
 })();
